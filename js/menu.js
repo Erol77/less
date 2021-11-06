998d392e04d253d8dc525938ae5b585825dbe139
Erol <veliev77@mail.ru>
2021-11-06 16:35:45 +0300

    added forEach from response to massiv

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,11 @@
 const restourant = 'food-band';
 const renderItems = (data) =>{
     console.log(data);
+     data.forEach((elem,index,array) => {
+          console.log(elem)});
 };
 // http://erol.qlihost.ru/db.json
-fetch(`http://erol.qlihost.ru/db.json`)
+fetch(`./db/${restourant}.json`)
     .then((response) => response.json())
     .then((data) => {
         renderItems(data);
