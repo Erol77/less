68ce8bc546a060a67d024bd7f542a8aaa0dacf88
Erol <veliev77@mail.ru>
2021-11-07 09:41:24 +0300

    added restourant cards to db

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,10 +4,34 @@ const menu = () => {
     const renderItems = (data) =>{
         console.log(data);
         data.forEach((elem,index,array) => {
-            console.log(elem)});
+            console.log(elem)
             const {description, id, image, name, price} = elem;
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = `
+                <img src="${image}" alt="${name}" class="card-image" />
+                <div class="card-text">
+                    <div class="card-heading">
+                        <h3 class="card-title card-title-reg">${name}</h3>
+                    </div>            
+                    <div class="card-info">
+                        <div class="ingredients">${description}
+                        </div>
+                    </div>
+                    <!-- /.card-info -->
+                    <div class="card-buttons">
+                        <button class="button button-primary button-add-cart">
+                            <span class="button-card-text">В корзину</span>
+                            <span class="button-cart-svg"></span>
+                        </button>
+                        <strong class="card-price-bold">${price} ₽</strong>
+                    </div>
+                </div>
+            `;
+cardsMenu.append(card);
+            });
     };
-    console.log(a.data);
+    //
     // http://erol.qlihost.ru/db.json
     fetch(`./db/${restourant}.json`)
         .then((response) => response.json())
