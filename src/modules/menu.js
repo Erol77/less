1dac56178ff15d1a5d9f2c91aa91d95b86fce143
Erol <veliev77@mail.ru>
2021-11-07 09:30:16 +0300

    added cards for index to js

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,11 +1,13 @@
-'use strict';
+const cardsMenu = document.querySelector('.cards-menu');
 const menu = () => {
     const restourant = 'food-band';
     const renderItems = (data) =>{
         console.log(data);
         data.forEach((elem,index,array) => {
             console.log(elem)});
+            const {description, id, image, name, price} = elem;
     };
+    console.log(a.data);
     // http://erol.qlihost.ru/db.json
     fetch(`./db/${restourant}.json`)
         .then((response) => response.json())
@@ -18,3 +20,43 @@ const menu = () => {
         // .finally()
 };
 export default menu;
+/*
+<main class="main">
+		<div class="container">
+			<section class="menu">
+				<div class="section-heading">
+					<h2 class="section-title restaurant-title">Пицца Плюс</h2>
+					<div class="card-info">
+						<div class="rating">
+							4.5
+						</div>
+						<div class="price">От 900 ₽</div>
+						<div class="category">Пицца</div>
+					</div>
+					<!-- /.card-info -->
+				</div>
+				<div class="cards cards-menu">
+					<div class="card">
+						<img src="img/pizza-plus/pizza-vesuvius.jpg" alt="image" class="card-image" />
+						<div class="card-text">
+							<div class="card-heading">
+								<h3 class="card-title card-title-reg">Пицца Везувий</h3>
+							</div>
+							<!-- /.card-heading -->
+							<div class="card-info">
+								<div class="ingredients">Соус томатный, сыр «Моцарелла», ветчина, пепперони, перец
+									«Халапенье», соус «Тобаско», томаты.
+								</div>
+							</div>
+							<!-- /.card-info -->
+							<div class="card-buttons">
+								<button class="button button-primary button-add-cart">
+									<span class="button-card-text">В корзину</span>
+									<span class="button-cart-svg"></span>
+								</button>
+								<strong class="card-price-bold">545 ₽</strong>
+							</div>
+						</div>
+						<!-- /.card-text -->
+					</div>
+					<!-- /.card --></div>*/
--- src/modules/partners.js
+++ src/modules/partners.js
@@ -1,10 +1,34 @@
 // const array = [11,22,34,65];
-'use strict';
+const cardsRestaurants = document.querySelector('.cards-restaurants');
+        //console.dir(cardsRestaurants);
 const partners = () => {    
-    const renderItems = (data) => {
-        console.log(data);
+    const renderItems = (data) => {       
         data.forEach((elem,index,array) => {
-        console.log(elem)});
+            const {image, kitchen, name, price, products,stars, time_of_delivery} = elem;
+
+            const a = document.createElement('a');
+            a.setAttribute('href','/restaurant.html');
+            a.classList.add('card');
+            a.classList.add('card-restaurant');
+            // a.products = products;
+            a.dataset.products = products;
+            a.innerHTML = `
+                    <img src="${image}" alt="image" class="card-image" />
+                        <div class="card-text">
+                            <div class="card-heading">
+                                <h3 class="card-title">${name}</h3>
+                                <span class="card-tag tag">${time_of_delivery} мин</span>
+                            </div>
+                            <div class="card-info">
+                                <div class="rating">
+                                    ${stars}
+                                </div>
+                                <div class="price">От ${price} ₽</div>
+                                <div class="category">${kitchen}</div>
+                            </div>
+                        </div>`;  
+        cardsRestaurants.append(a);
+        });
     };
 
     fetch('./db/partners.json')
@@ -17,8 +41,29 @@ const partners = () => {
         });
 };
 export default partners;
+/*
+<a href="restaurant.html" class="card card-restaurant">
+    <img src="img/pizza-plus/preview.jpg" alt="image" class="card-image" />
+    <div class="card-text">
+        <div class="card-heading">
+            <h3 class="card-title">Пицца плюс</h3>
+            <span class="card-tag tag">50 мин</span>
+        </div>
+        <!-- /.card-heading -->
+        <div class="card-info">
+            <div class="rating">
+                4.5
+            </div>
+            <div class="price">От 900 ₽</div>
+            <div class="category">Пицца</div>
+        </div>
+        <!-- /.card-info -->
+    </div>
+    <!-- /.card-text -->
+</a>
+<!-- /.card --></div>
 
-
+*/
 // array.forEach((elem,index,array) => {//в фу-ию приходит 3 аргумента, 1 элт ,2 его индех, 3 весь массив
 //     if (index ===3 ){        console.log(elem);    }});
 
