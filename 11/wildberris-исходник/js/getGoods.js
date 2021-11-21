71075b92322c832b612f083676c74a363ef31a6f
Erol <veliev77@mail.ru>
2021-11-21 19:34:11 +0300

    added cart & searh

--- 11/wildberris-исходник/js/getGoods.js
+++ 11/wildberris-исходник/js/getGoods.js
@@ -1,19 +1,54 @@
 const getGoods = function(){
     const links = document.querySelectorAll('.navigation-link');
+    const renderGoods = (goods) =>{
+        const goodsContainer = document.querySelector('.long-goods-list');
+        goodsContainer.innerHTML = '';
+        goods.forEach((good) =>{
+            const goodBlock = document.createElement('div');
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+            goodBlock.innerHTML =`
+                    <div class="goods-card">
+						<span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+					    <img src="db/${good.img}" alt="${good.name}"
+							class="goods-image">
+						<h3 class="goods-title">${good.name}</h3>
+	
+						<p class="goods-description">${good.description}</p>
+		
+						<button class="button goods-card-btn add-to-cart" data-id="007">
+							<span class="button-price">$${good.price}</span>
+						</button>
+					</div>
+                    `;
+                    // console.log('riva');
+                    goodsContainer.append(goodBlock);
+        });
+    };
 
-
-    const getData = () => {
+    const getData = (value,category) => {
             fetch('./db/db.json')
         .then(res => res.json())
-        .then((data)=>{console.log(data);
-            const el = data;
+        .then((data)=>{
+            const array = category ? data.filter((item) => item[category] === value) : data;
+            localStorage.setItem('goods', JSON.stringify(array));
+            // console.log(data);
+           if(window.location.pathname !== '/goods.html'){
+               window.location.href = 'goods.html';
+           } else { renderGoods(array);}
         });
     };
     links.forEach((link) =>{
         link.addEventListener('click', (e)=>{
             e.preventDefault();
-           localStorage.setItem('goods', JSON.stringify( getData()));
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+            getData(linkValue,category);
         });
     });
+
+    if(localStorage.getItem('goods') && window.location.pathname === '/goods.html'){
+        renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
 };
 getGoods();
